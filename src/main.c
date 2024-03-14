9f60ee9a7345b5c57d412b25f176f764d4afb139
VSXR <vsnzdev@gmail.com>
2024-03-14 10:48:05 +0100

    sdtbool added

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <pthread.h>  // concurrencia/multihilo
 #include <stdio.h>    // estándar de entrada/salida
 #include <stdlib.h>   // estándar de funciones de utilidad general
+#include <stdbool.h>  // tipos booleanos y valores true/false
 #include <string.h>   // manejo de cadenas de caracteres
 #include <ctype.h>    // funciones para manipular caracteres
 #include <time.h>     // manejo de tiempo
